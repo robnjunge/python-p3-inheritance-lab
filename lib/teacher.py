3717926ff6143f419635d20ad49045dca29ffa2f
robertnjunge <mubearobert@gmail.com>
2023-09-21 10:44:53 +0300

    completed assignment

--- lib/teacher.py
+++ lib/teacher.py
@@ -1,10 +1,18 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+
 
 from user import User
-
 import random
 
 class Teacher(User):
+    def __init__(self, first_name, last_name, knowledge=None):
+        super().__init__(first_name, last_name)
+        self.knowledge = knowledge if knowledge is not None else []
+
 
     def teach(self):
-        pass
+        if self.knowledge:
+            get_random = random.randint(0, len(self.knowledge) - 1)
+            return self.knowledge[get_random]
+        else:
+            raise Exception("No knowledge to teach")
